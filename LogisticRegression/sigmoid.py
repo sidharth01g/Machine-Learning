Corrected sign on equation

LogisticRegression/sigmoid.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def sigmoid(z):
-    return 1.0 / (1.0 + np.exp(z))
+    return 1.0 / (1.0 + np.exp(-z))
 
 
 def test():