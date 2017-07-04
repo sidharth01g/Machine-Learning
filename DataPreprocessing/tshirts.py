1f6808968fbb85c98e3e22b42057b6bd7acf317f
Sidharth <sidharth01g@gmail.com>
2017-07-05 00:53:24 +0530

    Added one-hot encoding

--- DataPreprocessing/tshirts.py
+++ DataPreprocessing/tshirts.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
 
 class Example(object):
 
@@ -56,6 +59,34 @@ class Example(object):
             self.df[self.class_label_heading].map(mapping)
         )
 
+    def one_hot_encode(self, feature_names_list):
+        """Encodes NOMINAL features using one-hot scheme.
+
+        Keyword arguments:
+        feature_indices_list -- list of indices of nominal features to
+            transform using one-hot encoding
+
+        """
+        X = self.df[:].values
+        le = LabelEncoder()
+
+        # Get feature_indices_list from feature_names_list
+        feature_indices_list = [
+            self.columns.index(feature_name) for feature_name in (
+                feature_names_list)]
+        for index in feature_indices_list:
+            # Encode labels corresponding to 'index' to integer values
+            X[:, index] = le.fit_transform(X[:, index])
+
+        print("Dataframe after integer labels encoding\n")
+        print(X, '\n')
+
+        ohe = OneHotEncoder(categorical_features=feature_indices_list)
+        transformed = ohe.fit_transform(X)
+        # print("Transformed:\n", transformed, '\n')
+        transformed_feature_vectors = transformed.toarray()
+        return transformed_feature_vectors
+
     @staticmethod
     def get_inverse_mapping(mapping):
         if type(mapping) is not dict:
@@ -81,10 +112,12 @@ def run():
     print("Dataframe after mapping SIZE to integers:\n")
     print(ex.get_dataframe(), '\n')
 
+    """
     inverse_size_mapping = {x: y for y, x in size_mapping.items()}
     ex.map_ordinal_feature(feature_name='Size', mapping=inverse_size_mapping)
     print("Dataframe after inverse-mapping SIZE:\n")
     print(ex.get_dataframe(), '\n')
+    """
 
     print("Printing class label mapping:\n")
     original_mapping = ex.get_class_mapping()
@@ -94,10 +127,18 @@ def run():
     ex.map_class_labels()
     print(ex.get_dataframe(), '\n')
 
-    print("Dataframe after restoring original mapping:\n")
+    """
+    print("Dataframe after restoring original class label mapping:\n")
     inverse_mapping = Example.get_inverse_mapping(original_mapping)
     ex.map_class_labels(mapping=inverse_mapping)
     print(ex.get_dataframe(), '\n')
+    """
+
+    features_to_encode_one_hot = ['Color']
+    encoded_vectors = ex.one_hot_encode(features_to_encode_one_hot)
+    print("One-hot encoded feature vectors (Encoding: %s):\n" %
+          str(features_to_encode_one_hot))
+    print(encoded_vectors, '\n')
 
 
 if __name__ == '__main__':
