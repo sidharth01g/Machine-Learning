bcc558bf1b5b6fab1287ed6fd42cea1ca5caf268
Sidharth <sidharth01g@gmail.com>
2017-07-12 09:50:59 +0530

    Added computation of total with-class scatter matrix

--- DimensionalityReduction/LinearDiscriminantAnalysis/lda_wine.py
+++ DimensionalityReduction/LinearDiscriminantAnalysis/lda_wine.py
@@ -37,14 +37,29 @@ def test():
     X_test_std = sc.transform(X_test)
 
     class_labels = np.unique(wine.df.iloc[:, 0])
-    feature_means = {}
+    class_means = []
 
     heading('Class means: ')
 
     for label in class_labels:
         mean_ = np.mean(X_train_std[y_train == label], axis=0)
         print('Class label ', label, ':\n\t', mean_, '\n')
-        feature_means[label] = mean_
+        class_means.append(mean_)
+
+    # Initialize within-class scatter matrix to zeros
+    d = X_train_std.shape[1]
+    s_w = np.zeros((d, d))
+
+    for label, class_mean in zip(class_labels, class_means):
+
+        for x in X_train_std[y_train == label]:
+            X_train_std_zero_mean = x - class_mean[label]
+            X_train_std_zero_mean_t = X_train_std_zero_mean.reshape(d, 1)
+            s_w += X_train_std_zero_mean_t.dot(X_train_std_zero_mean_t.T)
+
+    heading('Within-class scatter matrix (Sw): ')
+    pp.pprint(s_w)
+    print('\nShape: ', s_w.shape)
 
 
 if __name__ == '__main__':
