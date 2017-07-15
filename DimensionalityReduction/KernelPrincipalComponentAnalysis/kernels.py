413559e5626efeda9318bdc3ff265bf321adf994
Sidharth <sidharth01g@gmail.com>
2017-07-16 00:27:41 +0530

    Added method to return alphas, lambdas for kernel PCA

--- DimensionalityReduction/KernelPrincipalComponentAnalysis/kernels.py
+++ DimensionalityReduction/KernelPrincipalComponentAnalysis/kernels.py
@@ -26,3 +26,28 @@ def rbf_kernel_pca(X, gamma, n_components):
     )
 
     return X_principal_components
+
+
+def rbf_kernel_pca2(X, gamma, n_components):
+
+    # Compute squared distance pairs
+    squared_distances = pdist(X, 'sqeuclidean')
+    squared_distances_matrix = squareform(squared_distances)
+
+    # Compute symmetric kernel matrix
+    K = exp(-gamma * squared_distances_matrix)
+
+    # Center the kernel matrix using formula
+    n_vectors = K.shape[0]
+    ones_n = np.ones((n_vectors, n_vectors)) / n_vectors
+    K = K - ones_n.dot(K) - K.dot(ones_n) + ones_n.dot(K).dot(ones_n)
+    (eigenvalues, eigenvectors) = eigh(K)
+
+    # Pick the largest eigenvalues (last value is the largest)
+    alphas = np.column_stack(
+        (eigenvectors[:, -i] for i in range(1, n_components + 1))
+    )
+
+    lambdas = [eigenvalues[-i] for i in range(1, n_components + 1)]
+
+    return (alphas, lambdas)
