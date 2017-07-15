aebbb54398e6d46e7eac7e8332041ed9cd965ed4
Sidharth <sidharth01g@gmail.com>
2017-07-15 17:33:18 +0530

    Added utils

---
+++ DimensionalityReduction/Examples/utils/common_methods.py
@@ -0,0 +1,7 @@
+def heading(text, character='='):
+    if type(text) is not str:
+        heading('<INVALID_HEADING>')
+        return
+    print('\n')
+    print(text)
+    print(character * len(text))
