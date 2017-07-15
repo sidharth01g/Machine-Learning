e1b9efbbc3e5249372ec9f596ddee72684887a82
Sidharth <sidharth01g@gmail.com>
2017-07-16 00:26:58 +0530

    Added method to compute kernel PCS projections of test samples

--- DimensionalityReduction/Examples/utils/common_methods.py
+++ DimensionalityReduction/Examples/utils/common_methods.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def heading(text, character='='):
     if type(text) is not str:
         heading('<INVALID_HEADING>')
@@ -5,3 +8,12 @@ def heading(text, character='='):
     print('\n')
     print(text)
     print(character * len(text))
+
+
+def project_x(X_sample, X, gamma, alphas, lambdas):
+    pair_distance = np.array(
+        [np.sum(
+            (X_sample - row)**2 for row in X)]
+    )
+    k = np.exp(-gamma * pair_distance)
+    return k.dot(alphas / lambdas)
