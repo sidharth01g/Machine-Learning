0f82fef854dd076b60ec9a926b176b696b35bf70
Sidharth <sidharth01g@gmail.com>
2017-07-16 22:18:57 +0530

    Separate train and test samples

--- Unnamed/wisconsin_breast_cancer.py
+++ Unnamed/wisconsin_breast_cancer.py
@@ -6,6 +6,10 @@ parent_dir = os.path.abspath(os.path.join(dir_path, os.pardir))
 sys.path.insert(0, parent_dir)
 
 import pprint as pp
+
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
 import utils.common
 from utils.common import heading
 
@@ -40,6 +44,26 @@ def test():
     heading('Class labels:')
     pp.pprint(y)
     print('Shape: ', y.shape)
+
+    le = LabelEncoder()
+    y = le.fit_transform(y)
+
+    heading('Class labels (after encoding labels):')
+    pp.pprint(y)
+    print('Shape: ', y.shape)
+
+    (X_train, X_test, y_train, y_test) = train_test_split(
+        X, y, test_size=0.20, random_state=1
+    )
+
+    heading('Training feature vectors:')
+    pp.pprint(X_train)
+    print('Shape: ', X_train.shape)
+
+    heading('Training feature labels:')
+    pp.pprint(y_train)
+    print('Shape: ', y_train.shape)
+
 
 
 
