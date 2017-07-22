dc2011c5e23525d5f6936781e564768d0392708d
Sidharth <sidharth01g@gmail.com>
2017-07-22 18:03:44 +0530

    Confusion matrix

---
+++ EvaluationAndTuning/confusion_matrix.py
@@ -0,0 +1,88 @@
+import os
+import sys
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.abspath(os.path.join(dir_path, os.pardir))
+sys.path.insert(0, parent_dir)
+
+import numpy as np
+import pprint as pp
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+from utils.common import heading
+from utils.common import show_error
+from utils.datasets_custom import WineExample
+
+
+def interpolate(features, replacement, strategy='mean'):
+    features[features == replacement[0]] = np.nan
+    imputer = Imputer(missing_values=replacement[1], strategy=strategy, axis=0)
+    imputer.fit(features)
+    return imputer.transform(features)
+
+
+def get_data():
+    # Load dataset
+    wine = WineExample()
+    wine.fetch_data()
+
+    X = wine.df.iloc[:, 1:].values
+    y = wine.df.iloc[:, 0].values
+
+    (X_train, X_test, y_train, y_test) = train_test_split(
+        X, y, test_size=0.3, random_state=0
+    )
+
+    # Encode labels
+    le = LabelEncoder()
+    y_train = le.fit_transform(y_train)
+    y_test = le.fit_transform(y_test)
+
+    return (X_train, X_test, y_train, y_test)
+
+
+def main():
+
+    np.set_printoptions(threshold=np.nan)
+    np.set_printoptions(linewidth=200)
+
+    try:
+        (X_train, X_test, y_train, y_test) = get_data()
+    except Exception as error:
+        show_error('Getting data failed')
+        heading('Exception Trace:')
+        raise error
+
+    heading('Training vectors:')
+    pp.pprint(X_train)
+    print('Shape: ', X_train.shape)
+
+    heading('Training class labels:')
+    pp.pprint(y_train)
+    print('Shape: ', y_train.shape)
+
+    pipeline = Pipeline(
+        [
+            ('scaler', StandardScaler()),
+            ('classifier', SVC(random_state=1))
+        ]
+    )
+
+    pipeline.fit(X_train, y_train)
+    y_predict = pipeline.predict(X_test)
+
+    conf_matrix = confusion_matrix(y_true=y_test, y_pred=y_predict)
+
+    heading('Confusion Matrix:')
+    pp.pprint(conf_matrix)
+
+
+if __name__ == '__main__':
+    main()
