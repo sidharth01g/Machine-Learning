bf77302999f49371b47e339d45abc80cece6e445
Sidharth <sidharth01g@gmail.com>
2017-08-11 18:49:14 +0530

    Show digit samples

--- NeuralNetworks/mnist_pgm1.py
+++ NeuralNetworks/mnist_pgm1.py
@@ -5,6 +5,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.abspath(os.path.join(dir_path, os.pardir))
 sys.path.insert(0, parent_dir)
 
+import matplotlib.pyplot as plt
 import pprint as pp
 from utils.common import heading
 from utils.common import load_mnist_dataset
@@ -17,6 +18,19 @@ def run():
     heading('Trainiing data')
     print('X_train: ', X_train.shape)
     print('y_train: ', y_train.shape)
+
+    (fig, ax) = plt.subplots(nrows=2, ncols=5, sharex=True)
+    ax = ax.flatten()
+    for digit in range(10):
+        # plt.figure()
+        digit_serial = X_train[y_train == digit][0]
+        digit_reshaped = digit_serial.reshape(28, 28)
+        pp.pprint(digit_reshaped)
+        ax[digit].imshow(digit_reshaped, cmap='Greys', interpolation='nearest')
+        ax[digit].set_xticks([])
+        ax[digit].set_yticks([])
+
+    plt.show()
 
 
 if __name__ == '__main__':
