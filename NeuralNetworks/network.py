4479bd31e09c588578c84f8c5d240a7dcf9a3b69
Sidharth <sidharth01g@gmail.com>
2017-08-13 20:44:30 +0530

    Added feed-forward

--- NeuralNetworks/network.py
+++ NeuralNetworks/network.py
@@ -8,7 +8,7 @@ class NeuralNetwork(object):
         self.node_counts = node_counts
         self.weights = np.array(
             [
-                np.random.randn(self.node_counts[i], self.node_counts[i + 1])
+                np.random.randn(self.node_counts[i + 1], self.node_counts[i])
                 for i in range(self.n_layers)[: -1]
             ]
         )
@@ -23,8 +23,46 @@ class NeuralNetwork(object):
     def n_layers(self):
         return len(self.node_counts)
 
+    def sigmoid(self, z):
+        return 1.0 / (1.0 + np.exp(-z))
+
+    def feed_forward(self, x):
+        for i in range(self.n_layers - 1):
+            print('\nfeed_forward stage: ', i)
+            print(
+                'w: ', self.weights[i].shape,
+                ',  x: ', x.shape,
+                ',  b: ', self.biases[i].shape
+            )
+            x = self.sigmoid(
+                np.dot(self.weights[i], x)
+                + self.biases[i][np.newaxis].T
+            )
+        return x
+
 
 def test():
+    # Perform import specific to test() method
+    import os
+    import sys
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    parent_dir = os.path.abspath(os.path.join(dir_path, os.pardir))
+    sys.path.insert(0, parent_dir)
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from utils.common import heading
+    from utils.common import load_mnist_dataset
+
+    # Prepare training data
+    heading('Data preparation')
+    data_dir = os.path.join(parent_dir, 'downloads', 'MNIST')
+    print('Dataset directory: ', data_dir)
+    (X_train, y_train) = load_mnist_dataset(data_dir)
+    heading('Trainiing data')
+    print('X_train: ', X_train.shape)
+    print('y_train: ', y_train.shape)
+
+    # Instantiate neural network
     node_counts = [28*28, 20, 10]
     net = NeuralNetwork(node_counts)
     pp.pprint(net)
@@ -36,6 +74,15 @@ def test():
     print('Biases:')
     pp.pprint(net.biases)
     pp.pprint(net.biases[0].shape)
+
+    # Test feed_forward
+    heading('Feed-forward test')
+    x_sample = X_train[0][np.newaxis].T
+    print(x_sample.shape)
+    result = net.feed_forward(x_sample)
+    pp.pprint(result)
+    pp.pprint(result.shape)
+    exit('TEST')
 
 
 if __name__ == '__main__':
